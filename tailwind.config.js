ce7de1c9c0f8c8ec83ccae4cce8ba657468f7598
soniya-builds <soniyasmiles05@gmail.com>
2026-06-07 20:49:46 +0600

    Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0f766e',
+        primary: '#0f172a',
       },
       fontFamily: {
         'display': ['Poppins', 'sans-serif'],
